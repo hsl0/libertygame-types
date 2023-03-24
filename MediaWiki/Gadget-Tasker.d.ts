e1d3feea4b51654e2f4d863d453e3bb32a25cb37
hsl0 <hsl0@outlook.kr>
2023-03-25 07:23:39 +0900

    - Tasker 타입 수정  - npm clean 스크립트 변경

--- MediaWiki/Gadget-Tasker.d.ts
+++ MediaWiki/Gadget-Tasker.d.ts
@@ -1,14 +1,36 @@
 declare module 'ext.gadget.Tasker' {}
 
+type Task = () => void | PromiseLike<any>;
+
+interface TaskerError {
+	task: Task;
+	error: Error | unknown;
+}
+
 declare global {
-    function registerRenderer(renderer: unknown): void;
-    function registerRenderer(...renderers: Array<unknown>): void;
+	class Tasker {
+		constructor();
 
-    function registerHandler(renderer: unknown): void;
-    function registerHandler(...renderers: Array<unknown>): void;
+		push(items: Iterable<Task | PromiseLike<any>>): this;
+		push(...items: Array<Task | PromiseLike<any>>): this;
 
-    function registerTrigger(renderer: unknown): void;
-    function registerTrigger(...renderers: Array<unknown>): void;
+		wait(): Promise<unknown>;
+
+		waitSettled(): ReturnType<typeof Promise.allSettled>;
+
+		run(): this;
+
+		catch(handler: (errors: TaskerError[]) => any): this;
+	}
+
+	function registerRenderer(renderers: Iterable<Task | PromiseLike<any>>): void;
+	function registerRenderer(...renderers: Array<Task | PromiseLike<any>>): void;
+
+	function registerHandler(handlers: Iterable<Task | PromiseLike<any>>): void;
+	function registerHandler(...handlers: Array<Task | PromiseLike<any>>): void;
+
+	function registerTrigger(triggers: Iterable<Task | PromiseLike<any>>): void;
+	function registerTrigger(...triggers: Array<Task | PromiseLike<any>>): void;
 }
 
 export {};
